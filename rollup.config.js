0104832c4c4bfd4501550627703f2cc754a1b9cf
Avaer Kazmer <a@modules.io>
2021-05-08 15:20:48 -0400

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,39 @@
 import commonjs from "@rollup/plugin-commonjs";
 import {nodeResolve} from '@rollup/plugin-node-resolve';
 
-export default {
-  input: 'src/react.js',
-  output: {
-    dir: 'output',
-    format: 'es'
-  },
-  plugins: [
-    commonjs({
-      // non-CommonJS modules will be ignored, but you can also
-      // specifically include/exclude files
-      include: [ "./index.js", "node_modules/**" ], // Default: undefined
-
-      // if true then uses of `global` won't be dealt with by this plugin
-      ignoreGlobal: false, // Default: false
-
-      // if false then skip sourceMap generation for CommonJS modules
-      sourceMap: false // Default: true
-    }),
-    nodeResolve(),
-  ]
+const output = {
+  dir: 'output',
+  format: 'es'
 };
+const plugins = [
+  commonjs({
+    // non-CommonJS modules will be ignored, but you can also
+    // specifically include/exclude files
+    include: [ "./index.js", "node_modules/**" ], // Default: undefined
+
+    // if true then uses of `global` won't be dealt with by this plugin
+    ignoreGlobal: false, // Default: false
+
+    // if false then skip sourceMap generation for CommonJS modules
+    sourceMap: false // Default: true
+  }),
+  nodeResolve(),
+];
+
+export default [
+  {
+    input: 'src/react.js',
+    output,
+    plugins,
+  },
+  {
+    input: 'src/react-dom.js',
+    output,
+    plugins,
+  },
+  {
+    input: 'src/react-three-fiber.js',
+    output,
+    plugins,
+  },
+];
