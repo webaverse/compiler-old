345e8041acb6c7d6926cacd40efd37714d6b2053
Avaer Kazmer <a@modules.io>
2021-05-08 08:29:33 -0400

    Create browser.js

---
+++ src/browser.js
@@ -0,0 +1,11 @@
+import reactThreeFiber from '@react-three/fiber';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import babelStandalone from 'babel-standalone';
+
+export {
+  reactThreeFiber,
+  React,
+  ReactDOM,
+  babelStandalone,
+};
