5e16f3706d1cd2fd9b282a532289ae54010e9d64
Avaer Kazmer <a@modules.io>
2021-05-08 09:18:11 -0400

    Create browser.js

---
+++ src/browser.js
@@ -0,0 +1,13 @@
+import reactThreeFiber from '@react-three/fiber';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import babelStandalone from 'babel-standalone';
+
+const browser = {
+  reactThreeFiber,
+  React,
+  ReactDOM,
+  babelStandalone,
+};
+console.log('load browser', browser);
+globalThis.browser = browser;
