4a82c866d1303ee62bf0ba875da6300eb8f6b684
Avaer Kazmer <a@modules.io>
2021-05-08 09:18:01 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-parcel build --no-cache --dist-dir dist browser.js
+parcel build --no-cache --dist-dir dist ./src/browser.js
 # cat prefix.js dist/browser/browser.js postfix.js >dist/browser/browser2.js
