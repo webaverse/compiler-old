06ff730c6b62b8284e956a1395e09be73790e514
Avaer Kazmer <a@modules.io>
2021-05-08 16:02:24 -0400

    Update react.js

--- src/react.js
+++ src/react.js
@@ -1,4 +1,4 @@
 import './node.js';
 export * from 'react';
-export {Element, Fragment, useState} from 'react';
+export {Element, Fragment, useState, useEffect, useRef} from 'react';
 export {default} from 'react';
